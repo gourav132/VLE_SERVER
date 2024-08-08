8297821943792b09fdf6840f0317007cf77e5aea
Gourav Chatterjee <gouravchatterjee65@gmail.com>
2024-08-08 17:06:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const { pool } = require("./Database/config");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4242;
+
+app.get("/", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM expenses");
+    client.release();
+    const data = result.rows;
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post("/recordExpense", async (req, res) => {
+  const { date, description, category, amount } = req.body;
+  try {
+    const client = await pool.connect();
+    await client.query(
+      `INSERT INTO expenses (date, description, category, amount) VALUES ($1, $2, $3, $4)`,
+      [date, description, category, amount]
+    );
+    client.release();
+    res.json({
+      status: "Entry added successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to add entry" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to http://localhost:${PORT}`);
+});
