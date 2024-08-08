47457f5af076c6ca4d9119c64e62432d2e503e8d
Gourav Chatterjee <gouravchatterjee65@gmail.com>
2024-08-08 18:59:24 +0530

    added cors support

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const { pool } = require("./Database/config");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors);
 
 const PORT = process.env.PORT || 4242;
 
