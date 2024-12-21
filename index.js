f75542883d95195b3e17ae216a8f7a08102cc3d9
Gourav Chatterjee <gouravchatterjee65@gmail.com>
2024-12-21 19:47:02 +0530

    bug related to package fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { pool } = require("./Database/config");
 const express = require("express");
 const cors = require("cors");
 
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const app = express();
